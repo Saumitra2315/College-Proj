6caef57be7718265f80ebb08e318e32c4cd48675
Saumitra2315 <saumitra.pandey2112@gmail.com>
2023-09-13 06:45:41 +0530

    Some changes in Quant Projects and foody

--- Devsoc/foody2/src/App.js
+++ Devsoc/foody2/src/App.js
@@ -1,11 +1,17 @@
 import Home from "./pages/Home";
 import Pages from "./pages/Pages";
+import Categories from "./components/Categories";
+import { BrowserRouter } from "react-router-dom";
 function App() {
   return (
     <div className="App">
-      <h1><Home/> </h1>
+      <BrowserRouter>
+      <Categories/>
+      <Pages/>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App;
+ 
