0f8b3ebf979f5fd86a2773d9d47be46c39762c88
Saumitra2315 <saumitra.pandey2112@gmail.com>
2023-10-18 18:44:47 +0530

    csis proj

---
+++ AuthXpert/authxpert/src/index.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import { ReactDOM } from 'react'
+import App from './App'
+function index() {
+  return (
+    <div>index</div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
